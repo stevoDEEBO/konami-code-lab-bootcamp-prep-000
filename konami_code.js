2e53b3dc8c7fa1f729e9e18ff92536e23bfba1b3
Stephen Telesmanic <steve.telesmanic@gmail.com>
2018-02-07 17:06:05 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,55 +1,33 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 
 function init() {
-  let index = 0;
+  // Write your JavaScript code inside the init() function
+let i = 0;
 
-  document.body.addEventListener('keydown', function(e) {
-    const key = parseInt(e.which || e.detail);
+//var body = document.querySelector('body')
+//var keys = ''
+document.body.addEventListener('keydown', function(e) {
+  var keys = parseInt(e.detail || e.which)
 
-    if (code[index] === key) {
-      index++;
+  //console.log(e.detail)
+  //console.log(e.location)
+  //console.log(e.which)
 
-      if (index === code.length) {
-        alert('Hurray!');
+  if (keys === code[i]) {
+    index ++
 
-        index = 0;
-      }
-    } else {
-      index = 0;
+    if (i === code.length - 1) {
+      alert("WOO HOO! You did it!")
+
+      i = 0
+      //keys = ''
     }
-  });
-}
+  }
 
-// const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
-//
-// function init() {
-//   // Write your JavaScript code inside the init() function
-// let i = 0;
-//
-// //var body = document.querySelector('body')
-// //var keys = ''
-// document.body.addEventListener('keydown', function(e) {
-//   var keys = parseInt(e.detail || e.which)
-//
-//   //console.log(e.detail)
-//   //console.log(e.location)
-//   //console.log(e.which)
-//
-//   if (keys === code[i]) {
-//     index ++
-//
-//     if (i === code.length - 1) {
-//       alert("WOO HOO! You did it!")
-//
-//       i = 0
-//       //keys = ''
-//     }
-//   }
-//
-//   else {
-//     i = 0
-//     //keys = ''
-//   }
-// })
-// }
-// //init();
+  else {
+    i = 0
+    //keys = ''
+  }
+})
+}
+//init();
