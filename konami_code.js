68db2ad8593c55c30e04dee905aa583b49a8c3da
Stephen Telesmanic <steve.telesmanic@gmail.com>
2018-02-07 16:43:19 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,5 +2,32 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   // Write your JavaScript code inside the init() function
+let i = 0
 
+var body = document.querySelector('body')
+const keys = ''
+body.addEventListener('keydown', function(e) {
+  keys = parseInt(e.detail || e.location || e.which)
+
+  console.log(e.detail)
+  console.log(e.location)
+  console.log(e.which)
+
+  if (keys === code[i]) {
+    index ++
+
+    if (i === code.length) {
+      alert("WOO HOO! You did it!")
+
+      i = 0
+      keys = ''
+    }
+  }
+
+  else {
+    i = 0
+    keys = ''
+  }
+})
 }
+init();
